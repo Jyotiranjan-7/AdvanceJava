2ee07eee62714eea19af176aad16a4f418c2c706
Jyotiranjan-7 <jyotiranjan.sahoo2003@gmail.com>
2026-07-27 22:02:06 +0530

    Initial commit

---
+++ src/main/java/com/example/connection/DBConnection.java
@@ -0,0 +1,32 @@
+package com.example.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/studentdb";
+
+    private static final String USER = "root";
+
+    private static final String PASSWORD = "Jyoti@2003"; // Change to your MySQL password
+
+    public static Connection getConnection() {
+
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
+
+        return null;
+    }
+
+}
